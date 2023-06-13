2afe4175cc493b49b2dd5a86eaf48d5039fb39bc
anonymous <thclark@mit.edu>
2023-06-13 08:56:50 -0400

    bits per char

---
+++ evaluation/bits_per_char.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import argparse
+from tqdm import tqdm
+import glob
+import re
+
+if __name__ == "__main__":
+    patt = "perps-cf-diff-sizes/20000000/1/*-REAL_REAL_full_results.csv"
+
+    for filename in glob.glob(patt):
+        lang = re.search("..?=(-REAL_REAL)")
+        assert lang
+        lang = lang.match
+        df = pd.read_csv(filename)
+        sum_surp = df.surprisal.sum()
+        sum_charlens = df.token.apply(len).sum()
+        bits_per_char = sum_surp / sum_charlens
+        print(f"Lang: {lang}, Bits per Char: {bits_per_char}")
